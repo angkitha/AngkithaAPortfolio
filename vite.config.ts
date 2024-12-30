b338907586ec58f89908938dace99c4bfd58e8d5
angkitha <aanguraj@ucsd.edu>
2024-12-29 23:28:23 -0800

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/TestPortfolio/',  // Ensure this is correct for GitHub Pages
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Make sure it outputs to the 'dist' folder
-    rollupOptions: {
-      output: {
-        // Ensure the files are generated in the correct format
-        entryFileNames: '[name]-[hash].js',
-        chunkFileNames: '[name]-[hash].js',
-        assetFileNames: '[name]-[hash].[ext]',
-      },
-    },
-  },
 });
