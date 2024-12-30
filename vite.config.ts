2714c05795d23af6386e0406dea5f9cc03c9812f
angkitha <aanguraj@ucsd.edu>
2024-12-29 23:10:44 -0800

    :)

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/TestPortfolio/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Output directory
-    rollupOptions: {
-      input: './index.html',  // Input file
-    },
-  },
 })
